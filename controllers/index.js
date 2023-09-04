a97ddb714b6806a34319f6c590ff8fa514fbdfbf
Brayden Day <braydenday99@gmail.com>
2023-09-04 09:42:43 -0600

    added envs , js routes and the index code for it

--- controllers/index.js
+++ controllers/index.js
@@ -6,5 +6,18 @@ const userRoutes = require("./api/userRoutes.js");
 router.use("/api/users",userRoutes)
 
 // will use blog routes from api folder
+const blogRoutes = require("./api/blogRoutes");
+router.use("/api/blogs",blogRoutes)
 
 // will use comment routes from api folder
+const commentRoutes = require("./api/commentRoutes");
+router.use("/api/comments",commentRoutes)
+
+const frontEnd = require("./frontendRoutes");
+router.use("/",frontEnd)
+
+router.get("/showsessions",(req,res)=>{
+    res.json(req.session)
+})
+
+module.exports = router;
